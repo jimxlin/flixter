265bc477ac8fa873957335e2294592fc762cabd1
Jim X. Lin <miljinx@gmail.com>
2017-01-21 22:44:13 -0800

    Add devise gem and sticky footer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'https://firehose-flixter-jim-lin.herokuapp.com/' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
